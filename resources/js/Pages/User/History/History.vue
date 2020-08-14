4ce27c93a9e509ede2e92bc3c1cb808ed6287570
Bintang Putra <chaostix@Bintangs-iMac.local>
2020-08-14 14:11:09 +0700

    Reupload on 23 January 2024

--- resources/js/Pages/User/History/History.vue
+++ resources/js/Pages/User/History/History.vue
@@ -153,7 +153,7 @@
                                     <div v-else-if="p.status_o === 'aktif'" class="media-right">
                                         <div class="text-primary">Active</div>
                                     </div>
-                                    <div class="media-title mb-1">Order #{{p.id_order}} (${{p.price}})</div>
+                                    <div class="media-title mb-1">Order #{{p.id_order}} (${{p.price}}) <div class="badge badge-secondary">{{p.kategori}} {{p.server}}</div></div>
                                     <div class="text-time">{{p.tgl_pesan}} ({{60 - p.diff}} Minutes Left)</div>
                                     <div class="media-description text-muted">Sell {{p.quantity}}G for (${{p.price}})
                                     </div>
