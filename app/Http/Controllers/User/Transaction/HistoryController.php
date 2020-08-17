1596b17df9d833e26322aac124a85cf2a5037ead
Bintang Putra <chaostix@Bintangs-iMac.local>
2020-08-17 11:53:41 +0700

    s

--- app/Http/Controllers/User/Transaction/HistoryController.php
+++ app/Http/Controllers/User/Transaction/HistoryController.php
@@ -38,7 +38,7 @@ class HistoryController extends Controller
         $req = $request->all();
         $req['tgl_pesan'] = date("Y-m-d");
         $req['id_order'] = rand(100000000, 9999999);
-        $req['status_o'] = 'aktif';
+//        $req['status_o'] = 'aktif';
         $do = (new Order())->insertRaw($req);
         return redirect(route('user.history'));
     }
