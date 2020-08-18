bff7bce9e271ea90360688924d7ec43d7e81c875
Bintang Putra <chaostix@Bintangs-iMac.local>
2020-08-18 13:51:40 +0700

    s

--- app/Data/Withdraw.php
+++ app/Data/Withdraw.php
@@ -21,6 +21,11 @@ class Withdraw extends Model
             ->get();
     }
 
+    public function getById(int $id)
+    {
+        return $this->db()->where('id', $id)->get()[0];
+    }
+
     public function getByIdUser($id_user)
     {
         return $this->db()->where('withdraw.id_user', $id_user)
@@ -28,21 +33,28 @@ class Withdraw extends Model
             ->orderBy('withdraw.id', 'desc')
             ->get();
     }
+
     public function hapus($id)
     {
-         $this->db()->where('id', $id)->delete();
+        $get = $this->getById($id);
+
+        $user = (new Users())->getById($get->id_user)[0];
+        (new Users())->updateRaw($user->id_user, ['saldo' => $get->dollar + $user->saldo]);
+        $this->db()->where('id', $id)->delete();
         event(new ItemEvents('test'));
         return true;
     }
+
     public function updateRaw($id, array $update)
     {
-         $this->db()->where('id', $id)->update($update);
+        $this->db()->where('id', $id)->update($update);
         event(new ItemEvents('test'));
         return true;
     }
+
     public function insertRaw(array $insert)
     {
-         $this->db()->insert($insert);
+        $this->db()->insert($insert);
         event(new ItemEvents('test'));
         return true;
     }
