6204ad3fa0292444a26cbf4fb4c0c0231e921bb9
Bintang Putra <chaostix@Bintangs-iMac.local>
2020-08-18 13:16:37 +0700

    Reupload on 23 January 2024

--- app/Transaction/Order.php
+++ app/Transaction/Order.php
@@ -29,7 +29,7 @@ class Order extends Model
             ->leftJoin('t_server', 't_items.id_item', '=', 't_server.id_item')
             ->leftJoin('t_kategori', 't_items.id_kategori', '=', 't_kategori.id_kategori')
             ->leftJoin('t_pengiriman', 't_items.id_pengiriman', '=', 't_pengiriman.id_pengiriman')
-            ->orderBy('t_order.created_at', 'asc')
+            ->orderBy('t_order.created_at', 'desc')
             ->get();
     }
 
@@ -41,7 +41,8 @@ class Order extends Model
                 ->leftJoin('t_server', 't_items.id_item', '=', 't_server.id_item')
                 ->leftJoin('t_kategori', 't_items.id_kategori', '=', 't_kategori.id_kategori')
                 ->leftJoin('t_pengiriman', 't_items.id_pengiriman', '=', 't_pengiriman.id_pengiriman')
-            ->get();
+                ->orderBy('t_order.created_at', 'desc')
+                ->get();
     }
     public function getById($id)
     {
