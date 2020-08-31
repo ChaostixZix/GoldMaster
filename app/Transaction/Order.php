5914437527e9115511b32dc854fdc3b6a6b9c0be
Bintang Putra <chaostix@Bintangs-iMac.local>
2020-08-31 11:16:01 +0700

    Reupload on 23 January 2024

--- app/Transaction/Order.php
+++ app/Transaction/Order.php
@@ -69,7 +69,7 @@ class Order extends Model
     public function insertRaw(array $insert)
     {
           date_default_timezone_set('Asia/Hong_Kong');
-        $insert['created_at'] = date('Y-m-d H:s:i');
+        $insert['created_at'] = date('Y-m-d H:i:s');
         $id = $this->db()->insertGetId($insert);
         event(new ItemEvents(['type' => 'order_baru', 'id' => $id]));
 
