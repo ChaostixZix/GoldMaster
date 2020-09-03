a061368f45e97530f8a509257021cf2efab74512
Bintang Putra <chaostix@Bintangs-iMac.local>
2020-09-03 18:13:39 +0700

    s

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,30 @@
 @include('layout.head')
 
 <body class="{{\Illuminate\Support\Facades\Session::get('usershow') ? 'layout-3' : ''}}">
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v8.0'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/id_ID/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your Chat Plugin code -->
+<div class="fb-customerchat"
+     attribution=setup_tool
+     page_id="111921780224642"
+     theme_color="#ffc300">
+</div>
 <div>
     <div class="main-wrapper {{\Illuminate\Support\Facades\Session::get('usershow') ? 'container' : 'main-wrapper-1
 '}}">
